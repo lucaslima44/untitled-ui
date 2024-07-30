78237f84af30e0441960716b412639127bffe040
lucaslima44 <lucaslima2011h@gmail.com>
2024-07-30 20:15:53 -0300

    fix: size of the company images

--- src/app/components/SectionCompanies/index.tsx
+++ src/app/components/SectionCompanies/index.tsx
@@ -18,35 +18,35 @@ export function SectionCompanies() {
           />
           <Image
             src="/sisyphus.webp"
-            className="w-[118px] h-[34px] xl:w-[146px] xl:h-[48px]"
+            className="w-[118px] h-[34px] xl:w-[169px] xl:h-[48px]"
             width={169}
             height={48}
             alt="Sisyphus"
           />
           <Image
             src="/circooles.webp"
-            className="w-[128px] h-[34px] xl:w-[146px] xl:h-[48px]"
+            className="w-[128px] h-[34px] xl:w-[183px] xl:h-[48px]"
             width={183}
             height={48}
             alt="Circooles"
           />
           <Image
             src="/catalog.webp"
-            className="w-[112px] h-[34px] xl:w-[146px] xl:h-[48px]"
+            className="w-[112px] h-[34px] xl:w-[160px] xl:h-[48px]"
             width={160}
             height={48}
             alt="Catalog"
           />
           <Image
             src="/quotient.webp"
-            className="w-[131px] h-[34px] xl:w-[146px] xl:h-[48px]"
+            className="w-[131px] h-[34px] xl:w-[187px] xl:h-[48px]"
             width={187}
             height={48}
             alt="Quotient"
           />
           <Image
             src="/hourglass.webp"
-            className="xl:hidden w-[136px] h-[34px] xl:w-[146px] xl:h-[48px]"
+            className="xl:hidden w-[136px] h-[34px] xl:w-[175px] xl:h-[48px]"
             width={175}
             height={48}
             alt="Hourglass"
